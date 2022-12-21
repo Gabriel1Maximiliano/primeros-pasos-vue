1dd9e20eb85263953345a498b6d3a2f9750508e0
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2022-12-20 22:51:27 -0300

    uso del interpolado de texto

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const app = Vue.createApp({
     // configuramnos vue 
-    template:`
-    <h1>Hola mundo</h1>
-    <p>Desde app.js</p>`
+    // template:`
+    // <h1>Hola mundo</h1>
+    // <p>Desde app.js</p>`
+   data(){
+    return{
+        author:'Lola',
+        quote:'Hola Mundo'
+    }
+   }
 })
 
 app.mount('#myApp');
